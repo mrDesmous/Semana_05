ac5fbdfb2902e46bd7fc0e2d55de976b1eee4ed3
unknown <Admin@IHR80PBF411.continental.edu.pe>
2019-11-12 22:28:54 -0500

    Products-AbousUS

--- app/src/main/java/com/example/desmous/MainActivity.java
+++ app/src/main/java/com/example/desmous/MainActivity.java
@@ -2,16 +2,45 @@ package com.example.desmous;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+        private Button btnProducts;
+        private  Button btnAboutUs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Enlazamos Activity + Layout
 
+        btnProducts = (Button) findViewById(R.id.btnEnter);
+        btnAboutUs = (Button) findViewById(R.id.btnAboutUs);
+
+        btnProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Log.i(Settings.INFO,"Products");
+                Intent i = new Intent(MainActivity.this,ProductListActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        btnAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.i(Settings.INFO, "About us");
+                Intent i = new Intent(MainActivity.this,AboutUsActivity.class);
+                startActivity(i);
+            }
+        });
     }
     @Override
     protected void onResume() {
